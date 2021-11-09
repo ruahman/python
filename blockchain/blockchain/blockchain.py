15084a56eef6ceb48d78635da0910790ed44b35d
Diego <ruahman@gmail.com>
2021-11-08 21:33:15 -0400

    blockchain

--- blockchain/blockchain/blockchain.py
+++ blockchain/blockchain/blockchain.py
@@ -1,33 +1,23 @@
-"""
-doc string
-"""
+"""Create a blockchain."""
 
-import datetime
-import hashlib
-import json
+import datetime  # datetime functions
+import hashlib  # for hashing
+import json  # for json
 from flask import Flask, jsonify
 
 
 class Blockchain:
-    """ class block string """
+    """Classic blockchain."""
 
     def __init__(self):
-        """ initialize chain """
-
+        """Initialize chain and set genesis block."""
         self.chain = []
 
         # create genesis block
         self.create_block(proof=1, previous_hash='0')
 
     def create_block(self, proof, previous_hash):
-        """ add block to the blockchain """
-
-        # create a block:
-        #   the previous_hash is for security, if someone tried
-        #   to change the data then the previous_hash of next block
-        #   would not match,
-        #   this is a way to make sure no one is messing with the block chain
-
+        """Add block to the blockchain."""
         block = {'index': len(self.chain) + 1,
                  'timestamp': str(datetime.datetime.now()),
                  'proof': proof,
@@ -37,22 +27,22 @@ class Blockchain:
         return block
 
     def get_previous_block(self):
-        """ get the latest block from the chain """
-        # get the last item in the chain
+        """Get the latest block from the chain."""
         return self.chain[-1]
 
     @staticmethod
     def proof_of_work(previous_proof):
-        """ get the proof of work before we add to the block """
-
+        """Get the proof of work before we add to the block."""
+        # initialize proof
         new_proof = 1
+
+        # turns true once proof is valid
         check_proof = False
 
         # loop till proof of work problem is solved
         while check_proof is False:
 
             # generate hash from new_proof and previous_proof
-            # math operation must me none symetrical
             hash_operation = hashlib.sha256(
                 str(new_proof**2 - previous_proof**2).encode()).hexdigest()
 
@@ -68,43 +58,41 @@ class Blockchain:
 
     @staticmethod
     def hash(block):
-        """ generate hash from block """
-
-        # encodeing that sha256 is expecting
+        """Retrurn hash from block."""
         encoded_block = json.dumps(block, sort_keys=True).encode()
 
         return hashlib.sha256(encoded_block).hexdigest()
 
     def is_chain_valid(self, chain):
-        """ check if entire block chain is valid """
-
+        """Check if entire block chain is valid."""
         # start with the genesis block
         previous_block = chain[0]
 
         block_index = 1
         while block_index < len(chain):
+            # next block
             block = chain[block_index]
 
             # if previous_hash does not match hash of previous block
             # then we know someone was messsing with the block chain
-            # hashing is hard to generate but easy to check
             if block['previous_hash'] != self.hash(previous_block):
                 return False
 
-            # check if proof is valid
-            #   get previous proof and current proof
+            # check if proof is valid.
             previous_proof = previous_block['proof']
             proof = block['proof']
 
             # verify proof
             hash_operation = hashlib.sha256(
                 str(proof**2 - previous_proof**2).encode()).hexdigest()
+
             if hash_operation[:4] != '0000':
                 return False
 
             # move on to the next block
             previous_block = block
             block_index += 1
+
         return True
 
 
@@ -117,9 +105,8 @@ blockchain = Blockchain()
 
 @app.route('/mine_block', methods=['GET'])
 def mine_block():
-    """ add a block to the blockchain """
-
-    # get previous block and it's proof
+    """Add a block to the blockchain."""
+    # get previous block and proof
     previous_block = blockchain.get_previous_block()
     previous_proof = previous_block['proof']
 
@@ -138,29 +125,30 @@ def mine_block():
                 'timestamp': block['timestamp'],
                 'proof': block['proof'],
                 'previous_hash': block['previous_hash']}
+
     return jsonify(response), 200
 
 
 @app.route('/get_chain', methods=['GET'])
 def get_chain():
-    """ get_chain """
+    """Retrun the blockchain."""
     response = {'chain': blockchain.chain,
                 'length': len(blockchain.chain)}
+
     return jsonify(response), 200
 
 
 @app.route('/is_valid', methods=['GET'])
 def is_valid():
-    """ check if blockchain is valid """
-
+    """Check if blockchain is valid."""
     is_chain_valid = blockchain.is_chain_valid(blockchain.chain)
+
     if is_chain_valid:
         response = {'message': 'block chain is valid'}
     else:
         response = {'message': 'block chain  is not valid'}
+
     return jsonify(response), 200
-
-
 
 
 # so that I can print json
