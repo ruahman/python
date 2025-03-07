cfaa32e4da2d2d57387d405dd09638cad7ec8703
Diego R Vila <dego_vila@yahoo.com>
2025-03-07 10:54:28 -0400

    add flake

---
+++ tutorial/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in 
+  {
+    devShells.${system}.default = with pkgs; mkShell {
+      buildInputs = [
+        python312
+        pyright
+        ruff
+        isort
+      ];
+     
+      shellHook = ''
+          echo "Welcome to python  "
+      '';
+    };
+  };
+}
